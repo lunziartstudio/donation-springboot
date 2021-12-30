112bb05bf1f29316966e17c277f94140c6e7942c
brunchking <francis545@yahoo.com.tw>
2021-12-30 22:11:59 +0800

    make it better

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/index")
+	public String greeting() {
+		return "index";
+	}
+}
