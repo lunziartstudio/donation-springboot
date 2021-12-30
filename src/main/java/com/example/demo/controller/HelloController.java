f0a844e95d11f6c57fe4d1dc48f61dd8043a542c
brunchking <francis545@yahoo.com.tw>
2021-12-30 22:14:00 +0800

    'fuckit'

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@RequestMapping("/index")
+	@GetMapping("/index")
 	public String greeting() {
 		return "index";
 	}
