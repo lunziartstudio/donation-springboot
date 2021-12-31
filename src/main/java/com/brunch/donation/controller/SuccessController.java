f93ec3aa80f9bfe1cdd62004734a4a06521df0ec
brunchking <francis545@yahoo.com.tw>
2021-12-31 22:33:26 +0800

    'test'

--- src/main/java/com/brunch/donation/controller/SuccessController.java
+++ src/main/java/com/brunch/donation/controller/SuccessController.java
@@ -1,11 +1,20 @@
 package com.brunch.donation.controller;
 
+import java.io.IOException;
+import java.net.URL;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.w3c.dom.Document;
+
+import ecpay.payment.integration.ecpayOperator.EcpayFunction;
 
 @RestController
 public class SuccessController {
+	protected String confPath = "/ecpay/payment/integration/config/EcpayPayment.xml";
+	protected Document doc;
+	
 	@GetMapping("/receive")
 	public ModelAndView success() {
 		ModelAndView mv = new ModelAndView();
@@ -13,14 +22,12 @@ public class SuccessController {
 		return mv;
 	}
 	
-	@GetMapping("nani2")
-	public String nani2() {
-		
-		return "nani2";
-	}
 	
 	@GetMapping("/nani")
-	public String nani() {
+	public String nani() throws IOException {
+		URL fileURL = this.getClass().getResource(confPath);
+		System.out.println(fileURL.getPath());
+		
 		return "nani2";
 	}
 }
