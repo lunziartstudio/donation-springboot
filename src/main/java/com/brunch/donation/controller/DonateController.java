2dedd7e6e0c398cce7296940d2bc5e065a6f6951
brunchking <francis545@yahoo.com.tw>
2022-01-08 00:45:34 +0800

    complete request form

--- src/main/java/com/brunch/donation/controller/DonateController.java
+++ src/main/java/com/brunch/donation/controller/DonateController.java
@@ -1,7 +1,12 @@
 package com.brunch.donation.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +21,13 @@ import com.brunch.donation.model.Streamer;
 import com.brunch.donation.repository.StreamerRepository;
 import com.brunch.donation.util.EcpayUtils;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 
 @RestController
 public class DonateController {
 	private static final Logger log = LoggerFactory.getLogger(DonateController.class);
-	
+
 	@Autowired
 	StreamerRepository streamerRepo;
 
@@ -29,9 +36,25 @@ public class DonateController {
 
 	@Autowired
 	EcpayUtils ecpayUtils;
-	
+
 	@PostMapping("/donate")
-	public String donation(@RequestBody DonationForm donationForm) {
+	public String donation(@RequestBody String paramStr) throws UnsupportedEncodingException {
+		DonationForm donationForm = new DonationForm();
+		Map<String, String> paramMap = new HashMap<String, String>();
+		String query = paramStr;
+		String[] pairs = query.split("&");
+		for (String pair : pairs) {
+			int idx = pair.indexOf("=");
+			paramMap.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"),
+					URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+		}
+
+//		System.out.println(queryMap);
+		donationForm.setName(StringUtils.defaultString(paramMap.get("name")));
+		donationForm.setPayment_method(StringUtils.defaultString(paramMap.get("payment_method")));
+		donationForm.setAmount(StringUtils.defaultString(paramMap.get("amount")));
+		donationForm.setMessage(StringUtils.defaultString(paramMap.get("message")));
+		donationForm.setTarget(StringUtils.defaultString(paramMap.get("target")));
 		getDonationFormDetail(donationForm);
 		if (Integer.parseInt(donationForm.getAmount()) < 0) {
 			log.error("amount < 0");
@@ -40,33 +63,31 @@ public class DonateController {
 		// init ecpay
 		EcpayUtils.initial();
 		String htmlPage = "";
-		
-		switch(donationForm.getPayment_method()) {
-			case "credit_card":
-				htmlPage = EcpayUtils.genAioCheckOutOneTime(config, donationForm);
-				break;
-			case "cvs_barcode":
-				htmlPage = EcpayUtils.genAioCheckOutCVS(config, donationForm);
-				break;
-			case "webATM":
-				htmlPage = EcpayUtils.genAioCheckOutWebATM(config, donationForm);
-				break;
-			case "ATM":
-				htmlPage = EcpayUtils.genAioCheckOutATM(config, donationForm);
-				break;
+
+		switch (donationForm.getPayment_method()) {
+		case "credit_card":
+			htmlPage = EcpayUtils.genAioCheckOutOneTime(config, donationForm);
+			break;
+		case "cvs_barcode":
+			htmlPage = EcpayUtils.genAioCheckOutCVS(config, donationForm);
+			break;
+		case "webATM":
+			htmlPage = EcpayUtils.genAioCheckOutWebATM(config, donationForm);
+			break;
+		case "ATM":
+			htmlPage = EcpayUtils.genAioCheckOutATM(config, donationForm);
+			break;
 //			case "ALL":
 //				htmlPage = EcpayUtils.genAioCheckOutALL(config, donationForm);
-			default:
-				break;
+		default:
+			break;
 		}
 		return htmlPage;
-		
+
 		// Query order
 //		System.out.println("queryTradeInfo: " + EcpayUtils.postQueryTradeInfo());
 //		return "/donate";
 	}
-	
-	
 
 	public void getDonationFormDetail(DonationForm donationForm) {
 		log.info(donationForm.getName());
